424deddb359a67e0c9e7e261ff3cfffbb603da22
SUVOM ROY <145280803+SuvomRoy@users.noreply.github.com>
2024-08-11 03:32:15 +0530

    Delete Assignment_week_03 directory

--- Assignment_week_03/Week 3_Spring Data JPA and Hibernate/Exercise_09/PrimaryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.primary.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.primary.model.Employee;
-
-public interface PrimaryRepository extends JpaRepository<Employee, Long> {
-}
