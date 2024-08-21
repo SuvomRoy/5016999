90087f6a39222dfc1e0141acf0637f16f2a08625
SUVOM ROY <145280803+SuvomRoy@users.noreply.github.com>
2024-08-21 18:12:56 +0530

    Delete Assignment_week_04&week_05 directory

--- Assignment_week_04&week_05/Week 4, 5_Spring REST/Exercise_01/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.bookstoreapi.repository;
-
-import com.example.bookstoreapi.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
--- Assignment_week_04&week_05/Week 4, 5_Spring REST/Exercise_03/BookRepository.java
+++
@@ -1,9 +0,0 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-// Custom query methods for filtering
-public interface BookRepository extends JpaRepository<Book, Long> {
-    List<Book> findByTitle(String title);
-    List<Book> findByAuthor(String author);
-    List<Book> findByTitleAndAuthor(String title, String author);
-}
